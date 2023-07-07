c0f44ac2d36d6dc33d30bfc7f85496ccff53d70a
Kirill <kirillvorobey8@gmail.com>
2023-07-07 10:58:38 +0300

    pre-version-1.06.1 Change in style and adding mobile adaptation. Changed the selection of available seats on the booking page. Added mobile adaptation of the home page.

--- eyelashes/client/static/client/scripts/book.js
+++ eyelashes/client/static/client/scripts/book.js
@@ -8,3 +8,15 @@ function chooseMonth(){
         }
     }
 }
+
+function selectDay(item){
+    if (item.nextElementSibling.getAttribute("hidden") !== null){
+        item.nextElementSibling.removeAttribute("hidden");
+    } else {
+        item.nextElementSibling.setAttribute("hidden", "");
+    }
+}
+
+function deselectDay(item){
+    item.setAttribute("hidden", "");
+}
